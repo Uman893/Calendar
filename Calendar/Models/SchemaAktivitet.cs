f6947be575e24f8dc1564b1c70cb50b82c762ac3
David Marin Aguilar <davidcse@hotmail.com>
2023-02-16 22:57:08 +0100

    Add project files.

---
+++ Calendar/Models/SchemaAktivitet.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+
+namespace Calendar.Models
+{
+    public class SchemaActivity
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime? Start { get; set; }
+        [Required]
+        public DateTime? End { get; set; }
+        public DateTime? ExpiresAt { get; set; }
+        //Iterations
+        //public virtual Schema? Schema { get; set; }
+        //[ForeignKey(nameof(Schema))]
+        //public int SchemaId { get; set; }
+        //public virtual Activity? Activity { get; set; }
+        //[ForeignKey(nameof(Activity))]
+        //public int ActivityId { get; set; }
+
+        [StringLength(10)]
+        public string? ThemeColor { get; set; }
+
+        public bool? IsFullDay { get; set; }
+    }
+}
